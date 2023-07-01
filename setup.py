bbfc9ce09338bcb8680deef7c7f293ab4e3bb2f1
axisrow <93047788+axisrow@users.noreply.github.com>
2023-07-01 22:55:09 +0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+setup(
+    name='Teleaxispy',
+    version='0.1',
+    description='A simple Telegram API wrapper',
+    url='https://github.com/axisrow/Teleaxispy',
+    author='axisrow',
+    author_email='axisrow@gmail.com',
+    license='MIT',
+    packages=['Teleaxispy'],
+    install_requires=[
+        'requests',
+    ],
+    zip_safe=False
+)
